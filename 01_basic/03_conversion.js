47e5d002fd0eaed55d9730fe16ded0e3f9331b61
yogesh gupta <YOGESHG1409@GMAIL.COM>
2025-03-18 18:25:31 +0000

    conversion comit

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,37 @@
+let score="yogesh"
+
+//console.log(typeof score); string
+//console.log(typeof (score));string
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); number
+//console.log(valueInNumber);  NAN
+
+//"33" => 33
+//"33anc"=> NaN
+// true=> 1; false=>0
+
+let isloggedIn ="yogesh"
+
+let bool= Boolean(isloggedIn)
+
+// console.log(typeof bool);
+// console.log(bool);
+
+// 1=>true; false=>0
+// "" => false
+// " yogesh" => true
+
+let somenumber =33
+
+let stringnumber = String(somenumber)
+
+// console.log(stringnumber); 33
+// console.log(typeof stringnumber); string
+
+
+
+
+
+
+
