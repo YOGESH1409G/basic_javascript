6d561f4bba0de3a08a37d101a6c9b0d924113315
yogesh gupta <YOGESHG1409@GMAIL.COM>
2025-03-18 17:37:56 +0000

    datatype comit

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)// we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("yogesh");
+
+let name="yogesh"
+let age= 18
+let isage = false
+
+// number=> 2 to the power 53
+// bigint
+// string =>""
+//boolean=> true/false
+// null => standalone value
+// undefined =>
+    // symbol=> unique
+
+
+//object
+
+console.log(typeof "yogesh");
+console.log(typeof undefined);  // indefined
+console.log(typeof null); // object
+
+
+
