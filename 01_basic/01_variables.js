011f24f4902b1037b85e04560d57af37fd73bca8
yogesh gupta <YOGESHG1409@GMAIL.COM>
2025-03-18 16:43:09 +0000

    variables comit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 144553
+let accountEmail = "yogeshg@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+// accountId = 2// not allowed
+
+accountEmail=" bhumica@gmail.com"
+accountPassword ="2112121"
+accountCity="bombay"
+
+console.log(accountEmail);
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
+/* prefer not to use var because of 
+issue in block scope and functional scope*/
